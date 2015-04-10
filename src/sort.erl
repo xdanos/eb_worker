f4d8ed8232c736f2e66ffab8ca80665db7faf77e
xtovarn <xtovarn@fi.muni.cz>
2015-04-10 13:34:32 +0200

    * implement this

---
+++ src/sort.erl
@@ -0,0 +1,52 @@
+%%%-------------------------------------------------------------------
+%%% @author xtovarn
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 10. IV 2015 13:33
+%%%-------------------------------------------------------------------
+-module(sort).
+-author("xtovarn").
+
+%% API
+-export([]).
+
+%% public static int[] merge(int[] a, int[] b) {
+%%
+%% int[] answer = new int[a.length() + b.length()];
+%% int i = 0, j = 0, k = 0;
+%% while (i < a.length() && j < b.length())
+%% {
+%% if (a[i] < b[j])
+%% {
+%% answer[k] = a[i];
+%% i++;
+%% }
+%% else
+%% {
+%% answer[k] = b[j];
+%% j++;
+%% }
+%% k++;
+%% }
+%%
+%% while (i < a.length())
+%% {
+%% answer[k] = a[i];
+%% i++;
+%% k++;
+%% }
+%%
+%% while (j < b.length())
+%% {
+%% answer[k] = b[j];
+%% j++;
+%% k++;
+%% }
+%%
+%% return answer;
+%% }
+
+implement_this() ->
+	ok.
