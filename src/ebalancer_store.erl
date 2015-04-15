3b542f6ea56a2b6526a9ca49e560e56e77244558
xtovarn <xtovarn@fi.muni.cz>
2015-04-15 16:59:45 +0200

    + init

--- src/ebalancer_store.erl
+++ src/ebalancer_store.erl
@@ -16,7 +16,7 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {}).
+-record(state, {store :: tid(), next_unique_id :: integer(), msg_counter :: integer(), msg_threshold :: integer()}).
 
 %%%===================================================================
 %%% API
@@ -26,10 +26,11 @@ start_link() ->
 	gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 init([]) ->
-	{ok, #state{}}.
+	{ok, #state{store = ets:new(undefined, [ordered_set]), next_unique_id = 1, msg_counter = 0, msg_threshold = 10}}.
 
-handle_call(_Request, _From, State) ->
-	{reply, ok, State}.
+handle_call({store, RawData}, _From, #state{store = Tab, next_unique_id = Id, msg_counter = N} = State) ->
+	ets:insert(Tab, {Id, RawData}),
+	{reply, ok, State#state{next_unique_id = N + 1, msg_counter = N + 1}}.
 
 handle_cast(_Request, State) ->
 	{noreply, State}.
